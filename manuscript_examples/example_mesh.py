7f34a8c5c86a281f3413803dbed98c304df61521
Edwin Dalmaijer <edwin.dalmaijer@gmail.com>
2021-02-08 00:31:38 +0000

    Changed manuscript MeSH example to endocrine gland neoplasms

--- manuscript_examples/example_mesh.py
+++ manuscript_examples/example_mesh.py
@@ -2,22 +2,35 @@ import os
 from bibliobanana import compute_yearly_citations, load_results_from_file, \
     plot_yearly_count
 
+# MeSH terms of interest:
+# Endocrine Gland Neoplasms [C04.588.322]
+#    Adrenal Gland Neoplasms [C04.588.322.078]
+#    Multiple Endocrine Neoplasia [C04.588.322.400]
+#    Ovarian Neoplasms [C04.588.322.455]
+#    Pancreatic Neoplasms [C04.588.322.475]
+#    Paraneoplastic Endocrine Syndromes [C04.588.322.490]
+#    Parathyroid Neoplasms [C04.588.322.525]
+#    Pituitary Neoplasms [C04.588.322.609]
+#    Testicular Neoplasms [C04.588.322.762]
+#    Thyroid Neoplasms [C04.588.322.894]
+
 # Define the search terms of interest.
 search_terms = [ \
-    "Denmark", \
-    "Finland", \
-    "Iceland", \
-    "Norway", \
-    "Sweden", \
+    "Adrenal Gland Neoplasms", \
+    "Ovarian Neoplasms", \
+    "Pancreatic Neoplasms", \
+    "Pituitary Neoplasms", \
+    "Testicular Neoplasms", \
+    "Thyroid Neoplasms", \
     ]
 
 # Define the comperison terms.
-comparison_terms = ["Scandinavian and Nordic Countries"]
+comparison_terms = ["Endocrine Gland Neoplasms"]
 # Define the search range.
 start_date = 1945
 end_date = 2019
 # Construct the name of the file to which we should save the data.
-save_file = "MeSH-countries_{}-{}".format(start_date, end_date)
+save_file = "MeSH-neoplasms_{}-{}".format(start_date, end_date)
 
 # Get the results from PubMed.
 if not os.path.isfile(save_file+".csv"):
